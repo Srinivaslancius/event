f6249914484d7d0045dc5bce105114a0b61589d5
srinivaslancuius <srinivas@lanciussolutions.com>
2017-09-20 17:44:06 +0530

    changes in register file

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 					<div class="background-footer"></div>
 					<div class="container">
 						<div class="row noo-footer-main">
-							<div class="col-md-3 col-sm-6">
+							<div class="col-md-4 col-sm-6">
 								<div class="widget widget_text">
 									<h4 class="widget-title">About Us</h4>
 									<div class="textwidget">
@@ -16,7 +16,9 @@
 									</div>
 								</div>
 							</div>
-							<div class="col-md-3 col-sm-6">
+
+
+							<!-- <div class="col-md-3 col-sm-6">
 								<div class="widget commerce widget_top_rated_products">
 									<h4 class="widget-title">Top Rated Products</h4>
 									<ul class="product_list_widget">
@@ -52,8 +54,8 @@
 										</li>
 									</ul>
 								</div>
-							</div>
-							<div class="col-md-3 col-sm-6">
+							</div> -->
+							<div class="col-md-4 col-sm-6">
 								<div class="widget tribe-events-adv-list-widget">
 									<h4 class="widget-title">Upcoming Events</h4>
 									<div class="tribe-mini-calendar-event">
@@ -106,7 +108,7 @@
 									</div>
 								</div>
 							</div>
-							<div class="col-md-3 col-sm-6">
+							<div class="col-md-4 col-sm-6">
 								<div class="widget mailchimp-widget">
 									<h4 class="widget-title">Subscribe</h4>
 									<form class="mc-subscribe-form">
@@ -183,151 +185,9 @@
 					</div>  
 				</div>  
 			</footer>  
-		</div>  
-		<div class="footer-player">
-			<div class="album-playlist">
-				<ul>
-					<li class="album-playlist-title">
-						<span class="playlist-play">
-							Play
-						</span>
-						<span class="playlist-release">
-							Play
-							<small>(Label)</small>
-						</span>
-						<span class="playlist-tract">
-							Track title
-							<small>(Track author)</small>
-						</span>
-						<span class="playlist-page">
-							Page
-						</span>
-						<span class="playlist-delete">
-							Delete
-						</span>
-					</li>
-					<li class="album-playlist-item" data-thumb="&lt;img width=&quot;150&quot; height=&quot;150&quot; src=_images/album/rock16.html alt=&quot;rock16&quot; /&gt;" data-name="Morning Tea" data-artist="Maroon 5 ft Wiz Khalifa">
-						<span class="playlist-play">
-							<a href="http://wp.nootheme.com/wemusic/wp-content/uploads/2015/06/MorningTea.mp3">
-								<i class="fa fa-play"></i>
-							</a>
-						</span>
-						<span class="playlist-release">
-							<a href="#">Live on Letterman</a>
-						</span>
-						<span class="playlist-tract">
-							<a href="#">Morning Tea</a>
-							<small><a href="javascript:void(0)">Maroon 5 ft Wiz Khalifa</a></small>
-						</span>
-						<span class="playlist-page">
-							<a href="#"><i class="fa fa-share"></i></a>
-						</span>
-						<span class="playlist-delete">
-							<a href="#"><i class="fa fa-times"></i></a>
-						</span>
-					</li>
-					<li class="album-playlist-item" data-thumb="&lt;img width=&quot;150&quot; height=&quot;150&quot; src=_images/album/rock16.html alt=&quot;rock16&quot; /&gt;" data-name="Time Space" data-artist="Engelbert Humperdinck">
-						<span class="playlist-play">
-							<a href="http://wp.nootheme.com/wemusic/wp-content/uploads/2015/06/TimeSpace.mp3"><i class="fa fa-play"></i></a>
-						</span>
-						<span class="playlist-release">
-							<a href="#">Live on Letterman</a>
-						</span>
-						<span class="playlist-tract">
-							<a href="#">Time Space</a>
-							<small><a href="javascript:void(0)">Engelbert Humperdinck</a></small>
-						</span>
-						<span class="playlist-page">
-							<a href="#"><i class="fa fa-share"></i></a>
-						</span>
-						<span class="playlist-delete">
-							<a href="#"><i class="fa fa-times"></i></a>
-						</span>
-					</li>
-					<li class="album-playlist-item" data-thumb="&lt;img width=&quot;150&quot; height=&quot;150&quot; src=_images/album/rock16.html alt=&quot;rock16&quot; /&gt;" data-name="Interlude" data-artist="Modern Talking">
-						<span class="playlist-play">
-							<a href="http://wp.nootheme.com/wemusic/wp-content/uploads/2015/06/Interlude.mp3"><i class="fa fa-play"></i></a>
-						</span>
-						<span class="playlist-release">
-							<a href="#">Live on Letterman</a>
-						</span>
-						<span class="playlist-tract">
-							<a href="#">Interlude</a>
-							<small><a href="javascript:void(0)">Modern Talking</a></small>
-						</span>
-						<span class="playlist-page">
-							<a href="#"><i class="fa fa-share"></i></a>
-						</span>
-						<span class="playlist-delete">
-							<a href="#"><i class="fa fa-times"></i></a>
-						</span>
-					</li>
-					<li class="album-playlist-item" data-thumb="&lt;img width=&quot;150&quot; height=&quot;150&quot; src=_images/album/rock16.html alt=&quot;rock16&quot; /&gt;" data-name="Straight Ahead" data-artist="The Wanted">
-						<span class="playlist-play">
-							<a href="http://wp.nootheme.com/wemusic/wp-content/uploads/2015/06/StraightAhead.mp3"><i class="fa fa-play"></i></a>
-						</span>
-						<span class="playlist-release">
-							<a href="#">Live on Letterman</a>
-						</span>
-						<span class="playlist-tract">
-							<a href="#">Straight Ahead</a>
-							<small><a href="javascript:void(0)">The Wanted</a></small>
-						</span>
-						<span class="playlist-page">
-							<a href="#"><i class="fa fa-share"></i></a>
-						</span>
-						<span class="playlist-delete">
-							<a href="#"><i class="fa fa-times"></i></a>
-						</span>
-					</li>
-					<li class="album-playlist-item" data-thumb="&lt;img width=&quot;150&quot; height=&quot;150&quot; src=_images/album/rock16.html alt=&quot;rock16&quot; /&gt;" data-name="Morning Prayer" data-artist="Helloween">
-						<span class="playlist-play">
-							<a href="http://wp.nootheme.com/wemusic/wp-content/uploads/2015/06/MorningPrayer.mp3"><i class="fa fa-play"></i></a>
-						</span>
-						<span class="playlist-release">
-							<a href="#">Live on Letterman</a>
-						</span>
-						<span class="playlist-tract">
-							<a href="#">Morning Prayer</a>
-							<small><a href="javascript:void(0)">Helloween</a></small>
-						</span>
-						<span class="playlist-page">
-							<a href="#"><i class="fa fa-share"></i></a>
-						</span>
-						<span class="playlist-delete">
-							<a href="#"><i class="fa fa-times"></i></a>
-						</span>
-					</li>
-					<li class="album-playlist-item" data-thumb="&lt;img width=&quot;150&quot; height=&quot;150&quot; src=_images/album/rock16.html alt=&quot;rock16&quot; /&gt;" data-name="Coming Down" data-artist="Shane Filan">
-						<span class="playlist-play">
-							<a href="http://wp.nootheme.com/wemusic/wp-content/uploads/2015/06/CominDown.mp3"><i class="fa fa-play"></i></a>
-						</span>
-						<span class="playlist-release">
-							<a href="#">Live on Letterman</a>
-						</span>
-						<span class="playlist-tract">
-							<a href="#">Coming Down</a>
-							<small><a href="javascript:void(0)">Shane Filan</a></small>
-						</span>
-						<span class="playlist-page">
-							<a href="#"><i class="fa fa-share"></i></a>
-						</span>
-						<span class="playlist-delete">
-							<a href="#"><i class="fa fa-times"></i></a>
-						</span>
-					</li>
-				</ul>
-			</div>
-			<div class="album-player">
-				<div class="album-player-embed">
-					<span class="contro-album-player contro-down-player"><i class="fa fa-angle-down"></i></span>
-					<span class="contro-album-player contro-up-player"><i class="fa fa-angle-up"></i></span>
-					<audio class="album-audio-embed">
-						<source src="#" type="audio/mpeg"/>
-					</audio>
-				</div>
-			</div>
 		</div>
+
+
 
 		<script type='text/javascript' src='js/code.jquery.com/jquery-1.11.3.min.js'></script>
 		<script type='text/javascript' src='js/jquery-migrate.min.js'></script>
