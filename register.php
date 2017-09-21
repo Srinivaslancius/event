d7283712139b2444c7a1eac3b5bd679dfb2710d4
Srinivaslancius <srinivas@lanciussolutions.com>
2017-09-21 14:15:05 +0530

    register chnaGES

--- register.php
+++ register.php
@@ -1,4 +1,112 @@
-<?php include_once "main_header.php"; ?>
+<!doctype html>
+<html lang="en-US">
+	
+<!-- Mirrored from tk-themes.net/html-wemusic/light/my-account.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 31 Aug 2017 10:03:30 GMT -->
+<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+ 		<link rel="shortcut icon" href="images/favicon.png"/>
+		<title>My Account | WeMusic - Music Band Event</title>
+
+		<link rel='stylesheet' href='css/settings.css' type='text/css' media='all'/>
+		<link rel='stylesheet' href='css/widget-calendar-full.css' type='text/css' media='all'/>
+		<link rel='stylesheet' href='css/style.css' type='text/css' media='all'/>
+		<link rel='stylesheet' href='css/commerce.css' type='text/css' media='all'/>
+		<link rel='stylesheet' href='css/font-awesome.min.css' type='text/css' media='all'/>
+		<link rel='stylesheet' href='css/jquery.mb.YTPlayer.css' type='text/css' media='all'/>
+		<link rel='stylesheet' href='css/owl.carousel.css' type='text/css' media='all'/>
+		<link rel='stylesheet' href='css/owl.theme.css' type='text/css' media='all'/>
+		<link rel='stylesheet' href='css/nivo-lightbox.css' type='text/css' media='all'/>
+		<link rel='stylesheet' href='css/nivo-default.css' type='text/css' media='all'/>
+		<link rel='stylesheet' href='css/mediaelementplayer.css' type='text/css' media='all'/>
+		<link rel='stylesheet' href='css/layout.css' type='text/css' media='all'/>
+
+		<link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Dosis:100,300,400,700,900,300italic,400italic,700italic,900italic' type='text/css' media='all'/>
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+	</head>
+	<body class="commerce-account theme-light">
+		<!--preloader-->
+        <div id="loading">
+			<div id="loading-center">
+				<div id="loading-center-absolute">
+					<div class="object"></div>
+					<div class="object"></div>
+					<div class="object"></div>
+					<div class="object"></div>
+					<div class="object"></div>
+					<div class="object"></div>
+					<div class="object"></div>
+					<div class="object"></div>
+					<div class="object"></div>
+					<div class="object"></div>
+				</div>
+			</div> 
+		</div>
+        <!--end preloader-->
+		<div class="site">
+			<header class="noo-header" id="noo-header">
+				<div class="navbar-wrapper">
+					<div class="navbar navbar-default navbar-static-top">
+						<div class="container">
+							<div class="navbar-header">
+								<h1 class="sr-only">Home</h1>
+								<a class="navbar-toggle collapsed" data-toggle="collapse" data-target=".noo-navbar-collapse">
+									<span class="sr-only">Navigation</span>
+									<i class="fa fa-bars"></i>
+								</a>
+								<a href="#" class="mobile-minicart-icon">
+									<i class="fa fa-shopping-cart"></i><span>0</span>
+								</a>
+								<a href="index.php" class="navbar-brand">
+									<img class="noo-logo-img noo-logo-normal" src="images/logo.png" alt="">
+									<img class="noo-logo-img noo-logo-floating" src="images/logo.png" alt="">
+								</a>
+							</div>  
+							<?php
+							    $currentFile = $_SERVER["PHP_SELF"];
+							    $parts = Explode('/', $currentFile);
+							    $page_name = $parts[count($parts) - 1];
+							?>
+							<nav class="collapse navbar-collapse noo-navbar-collapse">
+								<ul class="navbar-nav sf-menu">
+
+									<li class="<?php if($page_name == 'index.php') { ?>current-menu-item <?php } ?> menu-item-has-children">
+										<a href="index.php">Home</a>
+										<!-- <ul class="sub-menu">
+											<li><a href="index-video.html">Home video</a></li>
+											<li><a href="index-parallax.html">Home Parallax</a></li>
+											<li><a href="index-revolution.html">Home Revolution</a></li>
+										</ul> -->
+									</li>
+									<li class="<?php if($page_name == 'maesteos.php') { ?>current-menu-item <?php } ?> menu-item-has-children">
+										<a href="maesteos.php">MAESTOES</a>
+									</li>
+									<li class="<?php if($page_name == 'workshop.php') { ?>current-menu-item <?php } ?> menu-item-has-children">
+										<a href="workshop.php">WORKSHOPS</a>
+										<!-- <ul class="sub-menu">
+											<li><a href="workshop-detail.html">workshop-detail</a></li>
+											<li><a href="highlights.html">highlights</a></li>
+										</ul> -->
+									</li>
+									<li class="<?php if($page_name == 'register.php') { ?>current-menu-item <?php } ?> menu-item-has-children">
+										<a href="register.php">REGISTER</a>
+									</li>
+									<li class="<?php if($page_name == 'members.php') { ?>current-menu-item <?php } ?> menu-item-has-children"><a href="members.php">ABOUT DOHA</a></li>
+									<li class="<?php if($page_name == 'contact.php') { ?>current-menu-item <?php } ?> menu-item-has-children">
+										<a href="contact.php">CONTACT US</a>
+									</li>
+								</ul>
+							</nav>  
+						</div>  
+					</div>  
+				</div>
+			</header>
 			<div class="noo-page-heading">
 				<div class="container">
 					<div class="noo-page-breadcrumb">
@@ -66,190 +174,4 @@
 					</div> 
 				</div> 
 			</div>
-			<footer class="colophon site-info">
-				<div class="colophon wigetized">
-					<div class="background-footer"></div>
-					<div class="container">
-						<div class="row noo-footer-main">
-							<div class="col-md-3 col-sm-6">
-								<div class="widget widget_text">
-									<h4 class="widget-title">About Us</h4>
-									<div class="textwidget">
-										<p>
-											WeMusic was built for Music Band Event with selling ticket service directly on site. Quick way to popularize your events and make it more professional towards thousands of audiences.
-										</p>
-										<p>
-											With responsive layout and advanced function, all information is processed smoothly and designed to enhance event's ticket shopping experiences.
-										</p>
-									</div>
-								</div>
-							</div>
-							<div class="col-md-3 col-sm-6">
-								<div class="widget commerce widget_top_rated_products">
-									<h4 class="widget-title">Top Rated Products</h4>
-									<ul class="product_list_widget">
-										<li>
-											<a href="#">
-												<img width="100" height="100" src="images/album/rock9.jpg" alt="rock9"/>
-												<span class="product-title">Robinson Party</span>
-											</a>
-											<div class="star-rating">
-												<span style="width:100%"></span>
-											</div>
-											<span class="amount">&pound;30.00</span>
-										</li>
-										<li>
-											<a href="#">
-												<img width="100" height="100" src="images/album/rock13.jpg" alt="rock13"/>
-												<span class="product-title">Awesome Bass</span>
-											</a>
-											<div class="star-rating">
-												<span style="width:80%"></span>
-											</div>
-											<span class="amount">&pound;3,999.00</span>
-										</li>
-										<li>
-											<a href="#">
-												<img width="100" height="100" src="images/album/rock7.jpg" alt="rock7"/>
-												<span class="product-title">My life</span>
-											</a>
-											<div class="star-rating">
-												<span style="width:80%"></span>
-											</div>
-											<span class="amount">&pound;55.00</span>
-										</li>
-									</ul>
-								</div>
-							</div>
-							<div class="col-md-3 col-sm-6">
-								<div class="widget tribe-events-adv-list-widget">
-									<h4 class="widget-title">Upcoming Events</h4>
-									<div class="tribe-mini-calendar-event">
-										<div class="list-date">
-											<span class="list-dayname">Mon</span>
-											<span class="list-daynumber">28</span>
-										</div>
-										<div class="list-info">
-											<h2 class="entry-title summary">
-												<a href="#">Fiddler&#8217;s Green Amphitheatre</a>
-											</h2>
-											<div class="duration">
-												<span class="date-start dtstart">June 1-8:00 pm</span>
-												-
-												<span class="date-end dtend">October 31-10:00 pm</span>
-											</div>
-										</div>  
-									</div>
-									<div class="tribe-mini-calendar-event">
-										<div class="list-date">
-											<span class="list-dayname">Mon</span>
-											<span class="list-daynumber">28</span>
-										</div>
-										<div class="list-info">
-											<h2 class="entry-title summary">
-												<a href="#">Editions in the gallery cafe</a>
-											</h2>
-											<div class="duration">
-												<span class="date-start dtstart">June 1-9:30 pm</span>
-												-
-												<span class="date-end dtend">October 31-10:30 pm</span>
-											</div>
-										</div>  
-									</div>
-									<div class="tribe-mini-calendar-event">
-										<div class="list-date">
-											<span class="list-dayname">Mon</span>
-											<span class="list-daynumber">28</span>
-										</div>
-										<div class="list-info">
-											<h2 class="entry-title summary">
-												<a href="#">Ultra Music Festival</a>
-											</h2>
-											<div class="duration">
-												<span class="date-start dtstart">June 3-8:00 am</span>
-												-
-												<span class="date-end dtend">November 30-5:00 pm</span>
-											</div>
-										</div>  
-									</div>
-								</div>
-							</div>
-							<div class="col-md-3 col-sm-6">
-								<div class="widget mailchimp-widget">
-									<h4 class="widget-title">Subscribe</h4>
-									<form class="mc-subscribe-form">
-										<label>Pellentesque at condimentum ligula. Donec nisi nisi, porttitor a erat ac, fringilla ntum tellus.</label>
-										<div class="input-mail">
-											<input type="email" name="mc_email" class="mc-email" value="" placeholder="Enter your email here..."/>
-											<i class="fa fa-envelope-o icon-mc-email"></i>
-										</div>
-										<button disabled>Subscribe now</button>
-									</form>
-								</div>
-							</div>
-						</div>  
-						<div class="noo-footer-bottom">
-							<div class="widget widget_noo_infomation">
-								<ul class="noo-infomation">
-									<li>
-										<span class="fa fa-map-marker infomation-left"></span>
-										<address>South Avenue a 34521 New York, USA </address>
-									</li>
-									<li class="info-phone">
-										<span class="fa fa-phone infomation-left"></span>
-										<span>+84 123 456 789</span>
-									</li>
-									<li class="info-mail">
-										<span class="fa fa-envelope-o infomation-left"></span>
-										<span><a href="mailto:email@domain.com">email@domain.com</a></span>
-									</li>
-								</ul>
-							</div>
-							<div class="widget widget_noo_social">
-								<div class="noo_social">
-									<div class="social-all">
-										<span class="footer-social">
-											<span class="social-name">
-												Follow on facebook
-											</span>
-											<a href="#" class="fa fa-facebook"></a>
-										</span>
-										<span class="footer-social">
-											<span class="social-name">
-												Follow on google-plus
-											</span>
-											<a href="#" class="fa fa-google-plus"></a>
-										</span>
-										<span class="footer-social">
-											<span class="social-name">
-												Follow on twitter
-											</span>
-											<a href="#" class="fa fa-twitter"></a>
-										</span>
-										<span class="footer-social">
-											<span class="social-name">
-												Follow on youtube
-											</span>
-											<a href="#" class="fa fa-youtube"></a>
-										</span>
-										<span class="footer-social">
-											<span class="social-name">
-												Follow on skype
-											</span>
-											<a href="#" class="fa fa-skype"></a>
-										</span>
-										<span class="footer-social">
-											<span class="social-name">
-												Follow on linkedin
-											</span>
-											<a href="#" class="fa fa-linkedin"></a>
-										</span>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>  
-				</div>  
-			</footer>  
-		</div>  
-		<?php include_once "footer.php";?>
+			<?php include_once "footer.php";?>
