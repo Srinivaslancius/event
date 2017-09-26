dbf0da8113f2ab3adc1501c2357d5bd99c73ea06
Srinivaslancius <srinivas@lanciussolutions.com>
2017-09-26 16:51:44 +0530

    index page chnages

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 							</div>
 						</div>
 					</div> -->
-					<div class="row parallax row-fluid home-upcoming-event">
+					<!-- <div class="row parallax row-fluid home-upcoming-event">
 						<div class="overlay_parallax"></div>
 							<div class="container">
 								<div class="row">
@@ -237,7 +237,96 @@
 								    </div>
 								</div>
 							</div>
-						</div>
+						</div>-->
+
+						<div class="row parallax row-fluid home-upcoming-event">
+				
+					<div class="container">
+						<div class="row">			
+								<div class="noo-shortcode-events">
+									
+										<div class="row noo-team-contents" >
+											<div class="col-md-6 col-sm-6">
+												<div class="noo_team_item">
+													<div class="team_thumbnail">
+														<img src="images/doha_images/Image01_November_1.jpg" alt="team1"/>
+														<div class="team-info">
+															<h4 class="team_name">Workshops</h4>
+														</div>
+													</div>
+													<div class="team-detail">
+														<div class="team-detail-position">
+															<p class="ds" style="text-align: center">
+																Workshops
+															</p>
+															
+														</div>
+													</div>
+												</div>
+											</div>
+											
+												<div class="col-md-6 col-sm-6">
+													<div class="noo_team_item">
+														<div class="team_thumbnail">
+															<img src="images/doha_images/Image02_November_2.jpg" alt="team1"/>
+															<div class="team-info">
+																<h4 class="team_name">Workshops & Opening Milonga</h4>
+															</div>
+														</div>
+														<div class="team-detail">
+															<div class="team-detail-position">
+																<p class="ds" style="text-align: center">
+																	Workshops & Opening Milonga
+																</p>
+																
+															</div>
+														</div>
+													</div>
+												</div>
+
+												<div class="col-md-6 col-sm-6">
+													<div class="noo_team_item">
+														<div class="team_thumbnail">
+															<img src="images/doha_images/Image_3_november_3.jpg" alt="team1"/>
+															<div class="team-info">
+																<h4 class="team_name">Festival Night, Gala Dinner & Shows, Milonga</h4>
+															</div>
+														</div>
+														<div class="team-detail">
+															<div class="team-detail-position">
+																<p class="ds" style="text-align: center">
+																	Festival Night, Gala Dinner & Shows, Milonga
+																</p>
+																
+															</div>
+														</div>
+													</div>
+												</div>
+
+												<div class="col-md-6 col-sm-6">
+													<div class="noo_team_item">
+														<div class="team_thumbnail">
+															<img src="images/doha_images/Image04_Nvember4.jpg" alt="team1"/>
+															<div class="team-info">
+																<h4 class="team_name">Workshops & Farewell Milonga </h4>
+															</div>
+														</div>
+														<div class="team-detail">
+															<div class="team-detail-position">
+																<p class="ds" style="text-align: center">
+																	Workshops & Farewell Milonga 
+																</p>
+																
+															</div>
+														</div>
+													</div>
+												</div>
+
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
 					
 
 		<?php include_once "footer.php";?>
