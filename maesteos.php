94ca5a52218b4793976c343e31794479bf740d7b
Srinivaslancius <srinivas@lanciussolutions.com>
2017-09-26 18:22:00 +0530

    event site chnages

--- maesteos.php
+++ maesteos.php
@@ -82,23 +82,23 @@
 																	</p>
 																</div>
 															</li>
-															<!-- <li>
+															<li>
 																<div class="noo-sh-thumbail">
 																	<div class="shthumbail">
 																		<img width="600" height="450" src="images/blog/img2.jpg" alt="blog12"/>
 																	</div>
 																</div>
-																<div class="noo-sh-blog-content">
+																<!-- <div class="noo-sh-blog-content">
 																	<object class="embed-responsive-item"><iframe width="854" height="320" src="https://www.youtube.com/embed/Bi2sIsX_aX8" frameborder="0" allowfullscreen></iframe>
 																</object>
-																</div>
+																</div> -->
 																<div class="noo-sh-blog-content" style="width:100%">
 																	 <h3><a href="#">DJ</a></h3> 
 																	<p style="text-align:justify">
 																		Ala Malisa is a dancer, teacher and choreographer; devoted to dancing since earliest age (folk dance, ballet, modern jazz, broadway jazz, swing jazz, contemporary dance, contact improvisation), 2009-2013 leader of children’s dance group "Stonoga", 2011 graduated contemporary dance studies, 2007-2012 member of "House of Dance" Jazz Theatre and Physical Dance Theatre "Pracownia fizyczna"; since 5 years madly in love with tango dance and music - 2013 moved to Berlin just for tango; since that point intensely developing her tango skills in Germany, Argentina and Turkey. Her teachers were i.a. Ruben Feliz, Silvio Lavila, Sara Grdan&Ivan Terrazas, Janina Quinones, Noelia Hurtado, Stefania Colina&Juan Martin Cararra, Fatima Vitale.
 																	</p>
 																</div>
-															</li> -->
+															</li>
 										
 														</ul>
 													</div>
