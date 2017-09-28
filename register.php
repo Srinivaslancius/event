b53deff5399904a0499e6974eaaa9a8ff93e6469
lanciussrinivas <srinivas@lanciussolutions.com>
2017-09-28 15:31:26 +0530

    done changes in register page

--- register.php
+++ register.php
@@ -2,54 +2,45 @@
 			<div class="noo-page-heading-Workshop">
 				<img src="images/Doha-Skyline-Night-Cityscape-Paul-Reiffer-Professional-Landscape-Medium-Format-Phase-One-Photography-Photographer-UAE-Middle-East-Qatar.jpg" alt="blog12" style="width:100%; height:250px;"; />
 			</div>
-			<div class="row parallax row-fluid home-upcoming-event">
+			<div class="row parallax row-fluid home-upcoming-event" style="padding-bottom:12px">
 				<div class="overlay_parallax"></div>
 					<div class="container">
 						<div class="row">			
 								<div class="noo-shortcode-events">
 								<h2 class="sh-event-title">register</h2>
 									<div class="container-wrap">
-				<div class="container">
-					<div class="row">
-						<div class="noo-main col-md-12">
- 							<div class="commerce">
-								<div class="col2-set" id="customer_login">
-									<div class="col-md-3">
-									</div>
-									<div class="col-md-6">
-										<h2>Register</h2>
-										<form class="register">
-											<div class="form-row">
-												<label for="reg_email">
-													Email address <span class="required">*</span>
-												</label>
-												<input type="email" class="input-text" name="email" id="reg_email" value=""/>
-											</div>
-											<div class="form-row">
-												<label for="reg_password">
-													Password <span class="required">*</span>
-												</label>
-												<input type="password" class="input-text" name="password" id="reg_password"/>
-											</div>
-											<div class="form-row">
-												<input type="submit" class="button" name="register" value="Register" style="background-color:#d91f26" />
-											</div>
-										</form>
-									</div>
-									<div class="col-md-3">
-									</div>
-								</div>
-							</div>
-						</div>  
-					</div> 
-				</div> 
-			</div>
-									
 										
-									</div>
-								</div>
+											
+												
+													
+															
+						<center><div class="col-md-12 col-sm-12">
+						<a href="http://buytickets.at/tangoclubqatar/118379"><button class="button1 button3" style="background-color:#d91f26; color:white; margin-bottom:15px">Gala Dinner Tickets</button></a></div>
+						<div class="col-md-12 col-sm-12">
+					<a href="http://buytickets.at/tangoclubqatar/118379" style="background-color:transparent; color:black;"><button class="button1 button4">Regular 325 QAR<br>Incl Buffet</button></a>
+							<a href="http://buytickets.at/tangoclubqatar/118379" style="background-color:transparent; color:black;"><button class="button1 button4">VIP 425 QAR Incl Buffet<br>+ 2 selected Beverages</button></a>						
+						
+						</div></center>
+							<center><div class="col-md-12 col-sm-12">
+							<a href="http://buytickets.at/tangoclubqatar/118379"><button class="button1 button3" style="background-color:#d91f26; color:white; margin-bottom:15px; margin-top:15px">Workshop Registrations</button></a>
 							</div>
-						</div> 		
+							<div class="col-md-12 col-sm-12">
+							<a href="http://buytickets.at/tangoclubqatar/118379"style="background-color:transparent; color:black;"><button class="button1 button4">Gold</button></a>
+							<a href="http://buytickets.at/tangoclubqatar/118379"style="background-color:transparent; color:black;"><button class="button1 button4">Silver</button></a>
+							<a href="http://buytickets.at/tangoclubqatar/118379"style="background-color:transparent; color:black;"><button class="button1 button4">Bronze</button></a>
+							<a href="http://buytickets.at/tangoclubqatar/118379"style="background-color:transparent; color:black;"><button class="button1 button4">Milonga</button></a>
+							<a href="http://buytickets.at/tangoclubqatar/118379"style="background-color:transparent; color:black;"><button class="button1 button4">Per Lession</button></a>
+							</div>
+							
+							
+					
+									</div>	
+								</div>
+						</div>
+						
+					</div>
+			</div>
+					
 	
 		
 <?php include_once "footer.php";?>
